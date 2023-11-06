bc632c00113eb3096737e2aa8bc0305ed8c1006b
Nami <havenothorn@gmail.com>
2023-11-07 03:44:18 +0900

    feat: SearchForm 컴포넌트 구현

--- 3-component/src/App.jsx
+++ 3-component/src/App.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import Header from "./components/Header";
+import SearchForm from "./components/SearchForm";
 
 export default class App extends React.Component {
+  search(searchKeyword) {
+    console.log("TODO: search", searchKeyword);
+  }
+
+  handleReset() {
+    console.log("TODO: handleReset");
+  }
+
   render() {
-    return <Header title="검색" />;
+    return (
+      <>
+        <Header title="검색" />
+        <div className="container">
+          <SearchForm
+            onSubmit={(searchKeyword) => this.search(searchKeyword)}
+            onReset={() => this.handleReset()}
+          />
+        </div>
+      </>
+    );
   }
 }
