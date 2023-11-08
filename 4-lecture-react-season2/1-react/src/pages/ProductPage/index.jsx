5665121145f5b43725ec2a8ffd2dcdda02786b0a
Nami <havenothorn@gmail.com>
2023-11-08 17:29:29 +0900

    feat: 상품 목록 API 불러오기

--- 4-lecture-react-season2/1-react/src/pages/ProductPage/index.jsx
+++ 4-lecture-react-season2/1-react/src/pages/ProductPage/index.jsx
@@ -1,25 +1,46 @@
-import Title from "../../components/Title";
-import ProductItem from "../../components/ProductItem";
-import Navbar from "../../components/Navbar";
+import React from "react";
+import ProductApi from "shared/api/ProductApi";
 import Page from "../../components/Page";
+import Title from "../../components/Title";
+import Navbar from "../../components/Navbar";
+import ProductItem from "../../components/ProductItem";
 
-const fakeProduct = {
-  id: "CACDA421",
-  name: "해물 계란 라면",
-  price: 6000,
-  thumbnail: "../images/menu-해물계란라면.jpg",
-};
+class ProductPage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      productList: [],
+    };
+  }
 
-const ProductPage = () => (
-  <div className="ProductPage">
-    <Page header={<Title>메뉴목록</Title>} footer={<Navbar />}>
-      <ul>
-        <li>
-          <ProductItem product={fakeProduct} />
-        </li>
-      </ul>
-    </Page>
-  </div>
-);
+  async fetch() {
+    try {
+      const productList = await ProductApi.fetchProductList();
+      this.setState({ productList });
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  componentDidMount() {
+    this.fetch();
+  }
+
+  render() {
+    return (
+      <div className="ProductPage">
+        <Page header={<Title>메뉴목록</Title>} footer={<Navbar />}>
+          <ul>
+            {this.state.productList.map((product) => (
+              <li key={product.id}>
+                <ProductItem product={product} />
+              </li>
+            ))}
+          </ul>
+        </Page>
+      </div>
+    );
+  }
+}
 
 export default ProductPage;
