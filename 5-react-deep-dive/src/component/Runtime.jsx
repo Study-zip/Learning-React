da4a6e89f120a94b5ab249418975249894036414
Nami <havenothorn@gmail.com>
2024-01-20 16:04:06 +0900

    add: jsx runtime

---
+++ 5-react-deep-dive/src/component/Runtime.jsx
@@ -0,0 +1,9 @@
+'use strict'
+
+var _jsxRuntime = require('react/jsx-runtime')
+
+var Component = (0, _jsxRuntime.jsx)('div', {
+    children: (0, _jsxRuntime.jsx)('span', {
+        children: 'hello world'
+    })
+})
